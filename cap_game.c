d84fb9fcf321adc4640be1a80b3c006b0cb6d1a2
KaplanAlex <Kaplanalex04@gmail.com>
2018-04-24 00:50:14 -0500

    thinking about capacitance

--- cap_game.c
+++ cap_game.c
@@ -1,11 +1,16 @@
 #include <msp430.h>
 #include <stdint.h>
 
+#define INTERRUPT_INTERVAL 100            // Interrupt every .1ms for timing.
+
 // SPI
 uint8_t *dataptr;                         // Pointer which traverses the tx information
 unsigned int data_len = 24;               // The number of bytes to be transmitted
 unsigned int tx_count = 0;                // Track transmission count in interrupts
 
+// Capacitive Sensing
+unsigned int pulse_time = 0;
+uint8_t pulse_rx = 0;
 
 // Random initial colors - TODO initialize...
 uint8_t colors[] = {0x00, 0x00, 0x00, 0x00, \
@@ -42,8 +47,8 @@ void setup()
     /* Setup TA0 to generate 1ms interrupts. */
     TA0CTL |= TASSEL_2 + MC_2 + ID_0;               // Source from SMCLK, Continuous Mode
     
-    TA0CCTL0 |= CCIE;                               // CCR0 interrupt enabled
-    TA0CCR0 = 1000;                                 // Interrupt in .1ms
+    TA0CCTL0 |= CCIE;                               // CCR0 interrupt enabled.
+    TA0CCR0 = INTERRUPT_INTERVAL;                   // Interrupt in .1ms.
     
     /* Set all capacitive buttons to inputs and enable pull up resistors. */
     /* Up - P2.2  Right - P2.3  Down - P2.4 Left - P2.5  Middle - P2.6 */
@@ -62,15 +67,15 @@ void setup()
     P2SEL2 &= ~BIT1;
     
     TA1CTL |= TASSEL_2 + MC_1 + ID_0;         // Source from SMCLK, Up mode (to TA1CCR0).
+    TA1CCTL0 |= CCIE;                         // CCR0 interrupt enable for PWM sent flag.
+
+    /* Set PWM to toggle on and off with 10 millisecond period. Period needs to be long enough to prevent 
+     * overlapping pulses when capacitance is large. 
+     */
+    TA1CCR0 = 10000;
+    TA1CCR1 = 2000;                         // Off at CCR1. Needs to be long on enough to be detected.
+    TA1CCTL1 = OUTMOD_7;                    // Set the PWM mode to toggle on at CCR0 and reset at CCR1.
     
-    // DONT think we need this but in case uncomment //
-    //TA1CCTL0 |= CCIE;                         // CCR0 interrupt enabled
-
-    /* Set PWM to */
-    TA1CCR0 = 99;                             // 100 levels.
-    TA1CCR1 = 0;                              // CCR1 = 1/2 CCR2 for maximum duty cycle.
-    TA1CCTL1 = OUTMOD_3;                      // Set the PWM mode to toggle on at CCR1 and reset at CCR0.
-
 }
 
 /*
@@ -150,7 +155,48 @@ void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A1 (void)
 #endif
 {
     // debounce();
-    TA0CCR0 += 1000; // Re-trigger the interrupt in one ms
-    __bic_SR_register_on_exit(LPM0_bits);  // Exit low power mode 0
+    TA0CCR0 += INTERRUPT_INTERVAL;           // Re-trigger the interrupt in another interavl.
+    // A new pulse has been sent.
+    if (!pulse_time) {
+        
+    }
+    
+    
+    /* Why is this difficult?
+     * 1. Make sure that pulse dont overlap - i.e. that we don't match a received pulse with a really old sent pulse. this is solved by making sure the wait time is long enough (small duty cycle?) for full pulse to progagate (issue for very large capacitance)
+     
+        Also needs to be long enough to be detected!
+        
+        Its probably ok but... we could do something where we dont send another pulse until the last one is received? More robust.
+     
+     * 2. Are we debouncing? If we are then we have to do it for every button. This is annoying because we have to do it based on received time, which will be different for all buttons even when some are pressed.
+                - *Potential solution*:
+                    Have a byte for every button and increment the count in the byte.
+                    Dependent on if that button has received yet (another byte).
+                    When pulse time is reset to 0, pass the received byte to debounce and 
+                    use that to update debounced state (gotta come up with threshold also).
+     
+     * 3. Do we need to debounce this receive also? I guess it couldn't hurt. Its just a lot of 
+          steps.
+     *
+     *
+     */
+    
+    __bic_SR_register_on_exit(LPM0_bits);    // Exit low power mode 0.
+}
+
+
+/* Timer A1 interrupt service routine - called on pulse send. */
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void Timer_A1 (void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A1 (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    // Reset time from pulse send to receive to zero and pulse receive flag.
+    pulse_time = 0;
 }
 
